33351ff21ba1f37dda11e2b751b3830b7666fbb8
SergioChiquinta <serchivera76@gmail.com>
2025-07-14 18:05:47 -0500

    db.js -> Se agregó la variable de entorno DB_PORT

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,15 +1,14 @@
 
 require('dotenv').config(); // Carga las variables de entorno
 
-
-const express = require('express');
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect(err => {
